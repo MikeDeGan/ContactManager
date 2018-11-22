5b32e008ebc23c435efe610abe2ff3b15777d292
Mike DeGan <MikeDeGan@ongov.net>
2018-11-21 19:16:20 -0500

    completed section 4, state and context api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Contact from './components/Contact';
+import Contacts from './components/Contacts';
 import Header from './components/Header';
+import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -8,21 +9,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header branding="Contact Manager" />
-        <div className="container">
-          <Contact
-            name="john doe"
-            email="jdoe@gmail.com"
-            phone="222-222-2222"
-          />
-          <Contact
-            name="Karen doe"
-            email="karen@gmail.com"
-            phone="111-222-2222"
-          />
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <Contacts />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
