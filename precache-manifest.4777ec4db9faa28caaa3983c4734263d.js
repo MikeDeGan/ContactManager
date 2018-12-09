b63c3ecf12457612f00fce13863a72631faa55c0
Mike DeGan <mikedegan@gmail.com>
2018-12-09 18:03:33 -0500

    Updates

---
+++ precache-manifest.4777ec4db9faa28caaa3983c4734263d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "6400298a0e1b41387f84",
+    "url": "/contactmanager/static/js/main.6400298a.chunk.js"
+  },
+  {
+    "revision": "365e321f854535ac4a40",
+    "url": "/contactmanager/static/js/1.365e321f.chunk.js"
+  },
+  {
+    "revision": "6400298a0e1b41387f84",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "365e321f854535ac4a40",
+    "url": "/contactmanager/static/css/1.04e9983f.chunk.css"
+  },
+  {
+    "revision": "2a9f50e4cc5b3efa650a33485ff5dfa7",
+    "url": "/contactmanager/index.html"
+  }
+];
